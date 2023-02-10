df06a1a9736ed5c7f136ef32beda3cd2b2e77d79
Aadi Poddar <aadipoddarmail@gmail.com>
2023-02-10 13:37:29 +0530

    Setting up the Window with LWJGL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import jade.Window;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Window window = Window.get();
+        window.run();
     }
 }
